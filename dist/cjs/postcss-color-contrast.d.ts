7910b0592544a937d761a3bd4d117eef724ac4b9
wiisportsresort <devdoge1@gmail.com>
2022-08-10 11:47:15 -0400

    actually build the thing

--- dist/cjs/postcss-color-contrast.d.ts
+++ dist/cjs/postcss-color-contrast.d.ts
@@ -1,5 +1,5 @@
 import type { PluginCreator as PluginCreatorFull } from 'postcss';
 import { PluginCreatorStub } from './postcss';
-declare type PluginCreator<T> = any extends PluginCreatorFull ? PluginCreatorStub<T> : PluginCreatorFull<T>;
+declare type PluginCreator<T> = any extends PluginCreatorFull<T> ? PluginCreatorStub<T> : PluginCreatorFull<T>;
 declare const postcssColorContrast: PluginCreator<{}>;
 export default postcssColorContrast;
