1ceb754c59f5041a96a7fdd4e483c7dc7b2854f8
wiisportsresort <devdoge1@gmail.com>
2022-08-30 00:15:03 -0400

    actually build the darn package

--- dist/esm/color.d.ts
+++ dist/esm/color.d.ts
@@ -2,10 +2,10 @@
  * Many functions taken from https://www.w3.org/TR/css-color-4/#color-conversion-code.
  */
 export declare const D65: number[];
-export declare type SRGBA = [r: number, g: number, b: number, a: number] & Nominal<'SRGBA'>;
-export declare type SRGB = [r: number, g: number, b: number] & Nominal<'SRGB'>;
-export declare type LinSRGB = [r: number, g: number, b: number] & Nominal<'LinSRGB'>;
-export declare type XYZ = [x: number, y: number, z: number] & Nominal<'XYZ'>;
+export declare type SRGBA = [r: number, g: number, b: number, a: number];
+export declare type SRGB = [r: number, g: number, b: number];
+export declare type LinSRGB = [r: number, g: number, b: number];
+export declare type XYZ = [x: number, y: number, z: number];
 export declare const normalizeSRGB: <T extends number[]>(c: T) => T;
 export declare const denormalizeSRGB: <T extends number[]>(c: T) => T;
 export declare const convertSRGBtoLinSRGB: (rgb: SRGB) => LinSRGB;
